5b7f4f1f47e25f29d6347ff112797ec4a7707235
sayoncamara <sayon.camara@hotmail.com>
2026-03-06 20:31:49 +0100

    Fix langchain text splitter import

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 # RAG imports
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
